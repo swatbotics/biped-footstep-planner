0627597f2c3c64ae2a04cd3f118590cb5e0d9c3c
Keliang He <khe1@swarthmore.edu>
2013-03-28 16:36:48 -0400

    Initial Commit
    Code from Keliang He Directed Reading with Prof. Matt Zucker Spring
    2012, Swarthmore College

---
+++ include/bipedSearch.h
@@ -0,0 +1,31 @@
+#ifndef _BIPEDSEARCH_H_
+#define _BIPEDSEARCH_H_
+
+#include "biped_checker.h"
+
+class bipedSearch{
+private:
+    typedef std::vector<biped*> NodeArray;
+
+    NodeArray _queue;
+    size_t _expCount;
+    NodeArray _poppedNodes;
+    
+
+public:
+
+    bipedSearch();
+    bool queueEmpty();
+    void enqueue(float x, float y, float angle, foot ft, biped* pred, float costToCome, float costToGo);
+    void enqueue(biped* newNode);
+    biped* dequeue();
+    ~bipedSearch();
+	biped* search(float x, float y, float angle, float goalx, float goaly, float goalr, BipedChecker* checker, int maxDepth, int viewDepth);
+	biped* search(biped* initial, float goalx, float goaly, float goalr, BipedChecker* checker, bool heuristic, int maxDepth, int viewDepth);
+
+    size_t getCount();
+    void makeSVG(biped* goal, std::string filename, OccupancyGrid<unsigned char> grid, float goalx, float goaly, float goalr, bool path, int zoom=12);
+    
+};
+
+#endif
